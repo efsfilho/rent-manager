0bc952d9ad8032df05f96e34a09ec8141067c9bb
Eduardo Filho <efdsfilho@gmail.com>
2023-04-14 23:32:58 -0300

    notification component created

---
+++ src/components/Notification.vue
@@ -0,0 +1,73 @@
+<template>
+  <!-- <v-card>
+    <v-card-title>{{title}}</v-card-title>
+    <v-card-actions>
+      <v-btn color="red-darken-1">Excluir</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="blue-darken-1">Cancelar</v-btn>
+      <v-btn color="blue-darken-1">Salvar</v-btn>
+    </v-card-actions>
+    <v-card-text>
+      <slot></slot>
+    </v-card-text>
+  </v-card> -->
+
+
+  <div class=" ">
+
+    <v-snackbar
+      v-model="visible"
+      :color="typeColor"
+      variant="elevated"
+      location="top"
+      >
+      <!-- <div class="text-caption">Erro</div>  -->
+      <div class="text-subtitle-1">
+        <!-- <v-icon
+          start
+          icon="error"
+        ></v-icon> -->
+        {{ message }}
+      </div>
+      <!-- <p class="text-body-1">This is a snackbar measdasddsdsassage</p> 
+      <div class="text-subtitle-2">This is a snackbar message</div>  -->
+      <!-- <v-row>
+        <v-col cols="2">
+          <div class="text-body-1">Erro</div>
+          <div class="text-body-2">Erro</div>
+        </v-col>
+        <v-col cols="10">
+          
+          <div class="text-body-2">This is a snackbar message</div>
+        </v-col>
+      </v-row> -->
+      <!-- {{ message }} -->
+      <template v-slot:actions>
+        <v-btn
+          variant="text"
+          @click="closeNotification"
+        >
+          Fechar
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script lang="ts">
+
+  export default {
+    props: {
+      message: String,
+      typeColor: String,
+    },
+    data: () => ({
+      visible: true,
+    }),
+    methods:{
+      closeNotification() {
+        this.$emit("close");
+      }
+    }
+  }
+</script>
--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -1,47 +1,49 @@
 <template>
-  <!-- <v-card> -->
-    <v-layout>
+  <v-layout>
       <!-- <v-card class="mx-auto" variant="outlined"> -->
       <!-- <v-app-bar color="surface-variant" title="Application bar"></v-app-bar> -->
       <!-- <Menu></Menu> -->
       <right-menu></right-menu>
 
       <!-- <v-card class="mx-auto" max-width="344" variant="outlined"> -->
-      <v-main style="min-height: 400px;" >
-        <v-container >
-          <!-- <v-row  >
-            <v-col> -->
-
-              <router-view ></router-view>
-            <!-- </v-col>
-          </v-row> -->
-          
-        </v-container>
-      </v-main>
-    <!-- </v-card> -->
-  <!-- </v-card> -->
-    </v-layout>
-  <!-- <v-app>
-    <Menu></Menu>
-
-    <v-main class="bg-grey-lighten-3">
-      <v-container>
-        <v-row>
+    <v-main style="min-height: 400px;" >
+      <v-container >
+        <!-- <v-row>
           <v-col>
-            <Tenant></Tenant>
+            <v-btn @click="showNotification">Open Snackbar</v-btn>
+            <v-btn @click="notificationVisible = false">Close Snackbar</v-btn>
           </v-col>
-        </v-row>
+          <v-col> -->
+        <router-view ></router-view>
+          <!-- </v-col>
+        </v-row> -->
+        
+        <notification
+          v-if="notificationVisible"
+          :message="notificationMessage"
+          :type-color="notificationType"
+          @close="closeNotification"
+        >
+          <!-- type-color="notificationType" -->
+      </notification>
       </v-container>
     </v-main>
-  </v-app> -->
+
+  </v-layout>
 </template>
 
 <script lang="ts" >
+  import Notification from '@/components/Notification.vue';
   import RightMenu from '@/components/RightMenu.vue'
-  
+
+  type messageType = 'success' | 'info' | 'warning' | 'error';
+
   export default {
     data: () => ({
-
+      notificationVisible: false,
+      notificationType: '',
+      notificationMessage: '',
+      count: 0,
       items: [
         { title: 'Home', icon: 'mdi-home-city' },
         { title: 'My Account', icon: 'mdi-account' },
@@ -49,6 +51,36 @@
       ],
       rail: false,
     }),
-    components: { RightMenu }
+    provide() {
+      return {
+        showNotification: this.showNotification
+      }
+    },
+    methods: {
+      showNotification(type: messageType, message: string) {
+        if (this.notificationVisible) {
+          this.notificationVisible = false;
+        }
+
+        setTimeout(() => {
+          this.notificationVisible = true;
+        }, 300);
+        
+        this.notificationType = type;
+        this.notificationMessage = message
+      },
+      closeNotification() {
+        this.notificationVisible = false;
+      }
+    },
+    watch: {
+      notificationVisible() {
+        this.count++;
+      }
+    },
+    components: { 
+      Notification,
+      RightMenu,
+    }
   }
 </script>
